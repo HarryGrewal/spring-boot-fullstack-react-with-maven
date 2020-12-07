8389c3caf1931088c4e0e2d5ae9e2768912a5eaa
Harbans Grewal <harbans_grewal@hotmail.com>
2020-12-07 19:04:47 +0100

    init

---
+++ src/main/java/com/cognizant/fullstack/test/springbootfullstackreactwithmaven/SpringBootFullstackReactWithMavenApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.fullstack.test.springbootfullstackreactwithmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFullstackReactWithMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFullstackReactWithMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognizant/fullstack/test/springbootfullstackreactwithmaven/SpringBootFullstackReactWithMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.fullstack.test.springbootfullstackreactwithmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFullstackReactWithMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
