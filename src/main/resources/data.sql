a19b0543a3a40b61b3ef12a5527e14d6c647d586
Harbans Grewal <harbans_grewal@hotmail.com>
2020-12-09 00:58:05 +0100

    defined valid template for consuming rextester api

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -35,8 +35,8 @@ values ('Fizz Buzz',
        ('Reverse String',
         ('Write a function that reverses a string. The input string is given as an array of characters char[]. ') ||
         'For Example : ' ||
-        'Input: ["h","e","l","l","o"] ' ||
-        'Output: ["o","l","l","e","h"] ',
+        'Input: hello ' ||
+        'Output: olleh ',
         ('hello'),
         ('olleh\n'));
 
