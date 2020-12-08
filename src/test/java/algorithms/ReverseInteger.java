cee82f4c68b0bfd962a8e504945471434cfe04c5
Harbans Grewal <harbans_grewal@hotmail.com>
2020-12-08 20:27:41 +0100

    added test algorithms

--- src/test/java/algorithms/ReverseInteger.java
+++ src/test/java/algorithms/ReverseInteger.java
@@ -1,9 +1,6 @@
 package algorithms;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.*;
-import java.lang.*;
+import java.util.Scanner;
 
 class ReverseInteger {
 
